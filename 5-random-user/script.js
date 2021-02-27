3dfb6f4ea866afc6ae4902532514417ed9a6f7b4
Daniel Nash <dnl.nash@gmail.com>
2021-02-27 16:48:48 -0500

    A little bit of unix, a lot of pain! Generates a new random user on every page load, and uses the google maps api, key not included if I did that right, to generate an image of the map given the latitude and longitude of the user

--- 5-random-user/script.js
+++ 5-random-user/script.js
@@ -1,5 +1,5 @@
 const url = 'https://randomuser.me/api/';
-
+let searchurl="";
 const avatar = document.getElementById('avatar');
 const fullname = document.getElementById('fullname');
 const username = document.getElementById('username');
@@ -8,8 +8,17 @@ const city = document.getElementById('city');
 const state = document.getElementById('state');
 const latitude = document.getElementById('latitude');
 const longitude = document.getElementById('longitude');
+let mapsearch = document.getElementById('mapsearch').src;
 const btn = document.getElementById('btn');
-
+window.addEventListener("load", () => {
+  fetch(url)
+    .then(handleErrors)
+    .then(res => res.json())
+    .then(updateProfile)
+    .catch(err => {
+      console.error(err)
+    })
+});
 btn.addEventListener("click", () => {
   fetch(url)
     .then(handleErrors)
@@ -18,6 +27,7 @@ btn.addEventListener("click", () => {
     .catch(err => {
       console.error(err)
     })
+window.location.reload(true);
 });
 
 function handleErrors(res) {
@@ -37,5 +47,8 @@ function updateProfile(profile) {
   state.innerHTML = profile.results[0].location.state;
   latitude.innerHTML = profile.results[0].location.coordinates.latitude;
   longitude.innerHTML = profile.results[0].location.coordinates.longitude;
-  return 1;
+searchurl="https://maps.googleapis.com/maps/api/staticmap?zoom=1.0&sensor=false&maptype=satalite&size="+(window.screen.height*window.devicePixelRatio/2)+"x"+(window.screen.width*window.devicePixelRatio/2)+"&center="+(latitude.innerHTML)+","+(longitude.innerHTML);
+srcurl=encodeURIComponent(searchurl);
+fetch(window.location.protocol+"//"+window.location.hostname+":8081/"+srcurl);
+return 1;
 }
